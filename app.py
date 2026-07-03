5f54db7a45d0d9b8f1c51ebe410ddf6d04579c44
Nick-Kevin <nicblou23@gmail.com>
2026-07-03 04:38:16 +0400

    update : UI

--- app.py
+++ app.py
@@ -50,19 +50,39 @@ with st.sidebar:
 st.markdown("## 📊 Live Session Behavior")
 
 # Using a 3-column grid of inputs
-col1, col2, col3 = st.columns(3)
+col11, col21, col31 = st.columns(3)
 
-with col1:
+with col11:
     st.markdown("### 🗺️ Administrative pages")
     admin_page = st.number_input("Page visited", min_value=0, value=2, step=1)
     admin_duration = st.number_input("Time spent (seconds)", min_value=0, value=385)
 
-with col2:
+with col21:
     st.markdown("### ℹ️ Informational pages")
     info_page = st.number_input("Page number", min_value=0, value=0, step=1)
     info_duration = st.number_input("Time spent (seconds)", min_value=0, value=0)
 
-with col3:
+with col31:
     st.markdown("### 🏷️ Product related pages")
     product_page = st.number_input("Page number", min_value=0, value=1, step=1)
     product_duration = st.number_input("Time spent (seconds)", min_value=0, value=75)
+
+st.write("---")
+
+st.markdown("## 📈 Engagement metrics (Google Analytics)")
+col12, col22, col32 = st.columns(3)
+
+with col12:
+    bounce_rate = st.slider("Bounce Rate", min_value=0.0, max_value=0.2, value=0.07, format="%0.2f")
+with col22:
+    exit_rate = st.slider("Exit rate", min_value=0.0, max_value=0.2, value=0.04, format="%0.2f")
+with col32:
+    page_value = st.number_input("Page value ($)", min_value=0, value=15)
+
+st.write("---")
+
+st.markdown("## 🔮 Prediction output")
+
+if st.button("Estimate session cluster", type="primary"):
+    st.success("High purchase visitor")
+    st.snow()
